67857a2efae339f51a4ad27ce3f0f83d6f8b7256
shnovaj30101 <shnovaj30101@gmail.com>
2023-01-09 20:26:25 +0800

    finish nine_square.py

--- nine_square.py
+++ nine_square.py
@@ -85,23 +85,25 @@ class NineSquare:
 
                 self.add_chess_to_lattice(chess_pos[row_idx][col_idx], (row_idx, col_idx), chess_color, return_animate, source_board)
 
-    def add_chess_to_lattice(self, chess_num, board_pos, chess_color = Colors.blue.value, return_animate = False, source_board = None):
+    def add_chess_to_lattice(self, append_chess_num, board_pos, chess_color = Colors.blue.value, return_animate = False, source_board = None):
         if return_animate:
-            if chess_num == 0:
+            if append_chess_num == 0:
                 return []
 
             row_idx, col_idx = board_pos
              
             animate_list = []
-            chess_num += len(self.mobject_pos[row_idx][col_idx])
+            ori_chess_num = len(self.mobject_pos[row_idx][col_idx])
+            finish_chess_num = ori_chess_num + append_chess_num
 
-            for i in range(chess_num):
-                if i < len(self.mobject_pos[row_idx][col_idx]):
+
+            for i in range(finish_chess_num):
+                if i < ori_chess_num:
                     animate_list.append(
                         self.mobject_pos[row_idx][col_idx][i].animate.move_to(
                                     self.line_vgroup.get_center() + UP * 1 * self.square_h + LEFT * 1 * self.square_w \
                                             + self.square_w * col_idx * RIGHT +  self.square_h * row_idx * DOWN \
-                                            + 0.5 * 0.1 * self.square_w * (chess_num-1) * LEFT + 0.5 * 0.1 * self.square_h * (chess_num-1) * UP \
+                                            + 0.5 * 0.1 * self.square_w * (finish_chess_num-1) * LEFT + 0.5 * 0.1 * self.square_h * (finish_chess_num-1) * UP \
                                             + 0.1 * self.square_w * i * RIGHT + 0.1 * self.square_h * i * DOWN 
                                 )
                     )
@@ -115,7 +117,7 @@ class NineSquare:
                                 ).move_to(
                                     self.line_vgroup.get_center() + UP * 1 * self.square_h + LEFT * 1 * self.square_w \
                                             + self.square_w * col_idx * RIGHT +  self.square_h * row_idx * DOWN \
-                                            + 0.5 * 0.1 * self.square_w * (chess_num-1) * LEFT + 0.5 * 0.1 * self.square_h * (chess_num-1) * UP \
+                                            + 0.5 * 0.1 * self.square_w * (finish_chess_num-1) * LEFT + 0.5 * 0.1 * self.square_h * (finish_chess_num-1) * UP \
                                             + 0.1 * self.square_w * i * RIGHT + 0.1 * self.square_h * i * DOWN 
                                 )
                         chess_circle.set_z_index(i)
@@ -126,7 +128,7 @@ class NineSquare:
 
                         animate_list.append(FadeIn(chess_circle))
                     else:
-                        chess_circle_source = source_board.mobject_pos[row_idx][col_idx].pop(i-chess_num)
+                        chess_circle_source = source_board.mobject_pos[row_idx][col_idx].pop(i-finish_chess_num)
                         chess_circle_target = get_chess_circle(
                                     color = chess_circle_source[0].color,
                                     border_color= chess_circle_source[1].color,
@@ -134,10 +136,10 @@ class NineSquare:
                                 ).move_to(
                                     self.line_vgroup.get_center() + UP * 1 * self.square_h + LEFT * 1 * self.square_w \
                                             + self.square_w * col_idx * RIGHT +  self.square_h * row_idx * DOWN \
-                                            + 0.5 * 0.1 * self.square_w * (chess_num-1) * LEFT + 0.5 * 0.1 * self.square_h * (chess_num-1) * UP \
+                                            + 0.5 * 0.1 * self.square_w * (finish_chess_num-1) * LEFT + 0.5 * 0.1 * self.square_h * (finish_chess_num-1) * UP \
                                             + 0.1 * self.square_w * i * RIGHT + 0.1 * self.square_h * i * DOWN 
                                 )
-                        chess_circle_target.set_z_index(100)
+                        chess_circle_target.set_z_index(i)
                         animate_list.append(
                                 ReplacementTransform(chess_circle_source, chess_circle_target)
                         )
@@ -146,23 +148,37 @@ class NineSquare:
                                     chess_circle_target
                             )
 
+            if source_board:
+                remain_chess_num = len(source_board.mobject_pos[row_idx][col_idx])
+
+                for i in range(remain_chess_num):
+                    animate_list.append(
+                        source_board.mobject_pos[row_idx][col_idx][i].animate.move_to(
+                                    source_board.line_vgroup.get_center() + UP * 1 * source_board.square_h + LEFT * 1 * source_board.square_w \
+                                            + source_board.square_w * col_idx * RIGHT +  source_board.square_h * row_idx * DOWN \
+                                            + 0.5 * 0.1 * source_board.square_w * (remain_chess_num-1) * LEFT + 0.5 * 0.1 * source_board.square_h * (remain_chess_num-1) * UP \
+                                            + 0.1 * source_board.square_w * i * RIGHT + 0.1 * source_board.square_h * i * DOWN 
+                                )
+                    )
+
             return animate_list
 
         else:
-            if chess_num == 0:
-                return 
+            if append_chess_num == 0:
+                return
 
             row_idx, col_idx = board_pos
 
-            chess_num += len(self.mobject_pos[row_idx][col_idx])
+            ori_chess_num = len(self.mobject_pos[row_idx][col_idx])
+            finish_chess_num = ori_chess_num + append_chess_num
 
-            for i in range(chess_num):
+            for i in range(finish_chess_num):
 
-                if i < len(self.mobject_pos[row_idx][col_idx]):
+                if i < ori_chess_num:
                     self.mobject_pos[row_idx][col_idx][i].move_to(
                                 self.line_vgroup.get_center() + UP * 1 * self.square_h + LEFT * 1 * self.square_w \
                                         + self.square_w * col_idx * RIGHT +  self.square_h * row_idx * DOWN \
-                                        + 0.5 * 0.1 * self.square_w * (chess_num-1) * LEFT + 0.5 * 0.1 * self.square_h * (chess_num-1) * UP \
+                                        + 0.5 * 0.1 * self.square_w * (finish_chess_num-1) * LEFT + 0.5 * 0.1 * self.square_h * (finish_chess_num-1) * UP \
                                         + 0.1 * self.square_w * i * RIGHT + 0.1 * self.square_h * i * DOWN 
                             )
 
@@ -175,7 +191,7 @@ class NineSquare:
                                 ).move_to(
                                     self.line_vgroup.get_center() + UP * 1 * self.square_h + LEFT * 1 * self.square_w \
                                             + self.square_w * col_idx * RIGHT +  self.square_h * row_idx * DOWN \
-                                            + 0.5 * 0.1 * self.square_w * (chess_num-1) * LEFT + 0.5 * 0.1 * self.square_h * (chess_num-1) * UP \
+                                            + 0.5 * 0.1 * self.square_w * (finish_chess_num-1) * LEFT + 0.5 * 0.1 * self.square_h * (finish_chess_num-1) * UP \
                                             + 0.1 * self.square_w * i * RIGHT + 0.1 * self.square_h * i * DOWN 
                                 )
                         chess_circle.set_z_index(i)
@@ -185,11 +201,11 @@ class NineSquare:
                             )
 
                     else:
-                        chess_circle = source_board.mobject_pos[row_idx][col_idx].pop(i-chess_num)
+                        chess_circle = source_board.mobject_pos[row_idx][col_idx].pop(i-finish_chess_num)
                         chess_circle.move_to(
                                     self.line_vgroup.get_center() + UP * 1 * self.square_h + LEFT * 1 * self.square_w \
                                             + self.square_w * col_idx * RIGHT +  self.square_h * row_idx * DOWN \
-                                            + 0.5 * 0.1 * self.square_w * (chess_num-1) * LEFT + 0.5 * 0.1 * self.square_h * (chess_num-1) * UP \
+                                            + 0.5 * 0.1 * self.square_w * (finish_chess_num-1) * LEFT + 0.5 * 0.1 * self.square_h * (finish_chess_num-1) * UP \
                                             + 0.1 * self.square_w * i * RIGHT + 0.1 * self.square_h * i * DOWN 
                                 )
 
@@ -208,26 +224,191 @@ class NineSquare:
         self.scene.play(FadeIn(chess_vgroup))
 
     def move_to(self, pos):
-        self.line_vgroup.move_to(pos)
+        shift_dir = pos - self.line_vgroup.get_center()
+        self.line_vgroup.shift(shift_dir)
 
         chess_vgroup = VGroup()
         for row_idx, col_idx in it.product(range(3), range(3)):
             chess_vgroup.add(*self.mobject_pos[row_idx][col_idx])
 
-        chess_vgroup.move_to(pos)
+        chess_vgroup.shift(shift_dir)
 
-    def pos_rotate(self):
-        pass
+    def board_rotate(self, return_animate = False):
+        if return_animate:
+            animate_list = []
 
-    def animate_rotate(self):
-        pass
+            animate_list.append(
+                Rotate(self.line_vgroup, angle = TAU/4)    
+            )
 
-class nine_square_problem(Scene):
+            for row_idx, col_idx in it.product(range(3), range(3)):
+                if len(self.mobject_pos[row_idx][col_idx]) == 0:
+                    continue
+
+                chess_group = VGroup(*self.mobject_pos[row_idx][col_idx])
+                chess_group_destination = chess_group.copy().rotate(
+                    angle = TAU/4,
+                    about_point = self.line_vgroup.get_center(),
+                ).rotate(
+                    angle = -TAU/4,
+                )
+                animate_list.append(
+                    Transform(
+                        chess_group,
+                        chess_group_destination,
+                        path_func = utils.paths.path_along_arc(TAU / 4),
+                    )
+                )
+                
+
+            tmp = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+            ]
+            for row_idx, col_idx in it.product(range(3), range(3)):
+                if row_idx == 0 and col_idx == 0:
+                    tmp[2][0] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 0 and col_idx == 1:
+                    tmp[1][0] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 0 and col_idx == 2:
+                    tmp[0][0] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 1 and col_idx == 0:
+                    tmp[2][1] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 1 and col_idx == 1:
+                    tmp[1][1] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 1 and col_idx == 2:
+                    tmp[0][1] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 2 and col_idx == 0:
+                    tmp[2][2] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 2 and col_idx == 1:
+                    tmp[1][2] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 2 and col_idx == 2:
+                    tmp[0][2] = self.mobject_pos[row_idx][col_idx]
+            for row_idx, col_idx in it.product(range(3), range(3)):
+                self.mobject_pos[row_idx][col_idx] = tmp[row_idx][col_idx]
+
+            return animate_list
+        else:
+            tmp = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+            ]
+            for row_idx, col_idx in it.product(range(3), range(3)):
+                if row_idx == 0 and col_idx == 0:
+                    tmp[2][0] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 0 and col_idx == 1:
+                    tmp[1][0] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 0 and col_idx == 2:
+                    tmp[0][0] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 1 and col_idx == 0:
+                    tmp[2][1] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 1 and col_idx == 1:
+                    tmp[1][1] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 1 and col_idx == 2:
+                    tmp[0][1] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 2 and col_idx == 0:
+                    tmp[2][2] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 2 and col_idx == 1:
+                    tmp[1][2] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 2 and col_idx == 2:
+                    tmp[0][2] = self.mobject_pos[row_idx][col_idx]
+            for row_idx, col_idx in it.product(range(3), range(3)):
+                self.mobject_pos[row_idx][col_idx] = tmp[row_idx][col_idx]
+
+    def chess_left_shift(self, return_animate = False):
+        if return_animate:
+            animate_list = []
+
+            for row_idx, col_idx in it.product(range(3), range(3)):
+                if len(self.mobject_pos[row_idx][col_idx]) == 0:
+                    continue
+
+                chess_group = VGroup(*self.mobject_pos[row_idx][col_idx])
+                chess_group_destination = chess_group.copy()
+                if col_idx == 0:
+                    chess_group_destination.shift(2 * self.square_w * RIGHT)
+                    animate_list.append(
+                        Transform(
+                            chess_group,
+                            chess_group_destination,
+                            path_func = utils.paths.path_along_arc(TAU / 4),
+                        )
+                    )
+                else:
+                    chess_group_destination.shift(self.square_w * LEFT)
+                    animate_list.append(
+                        Transform(
+                            chess_group,
+                            chess_group_destination,
+                        )
+                    )
+
+            
+            tmp = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+            ]
+            for row_idx, col_idx in it.product(range(3), range(3)):
+                if row_idx == 0 and col_idx == 0:
+                    tmp[0][2] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 0 and col_idx == 1:
+                    tmp[0][0] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 0 and col_idx == 2:
+                    tmp[0][1] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 1 and col_idx == 0:
+                    tmp[1][2] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 1 and col_idx == 1:
+                    tmp[1][0] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 1 and col_idx == 2:
+                    tmp[1][1] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 2 and col_idx == 0:
+                    tmp[2][2] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 2 and col_idx == 1:
+                    tmp[2][0] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 2 and col_idx == 2:
+                    tmp[2][1] = self.mobject_pos[row_idx][col_idx]
+            for row_idx, col_idx in it.product(range(3), range(3)):
+                self.mobject_pos[row_idx][col_idx] = tmp[row_idx][col_idx]
+
+            return animate_list
+        else:
+            tmp = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+            ]
+            for row_idx, col_idx in it.product(range(3), range(3)):
+                if row_idx == 0 and col_idx == 0:
+                    tmp[0][2] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 0 and col_idx == 1:
+                    tmp[0][0] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 0 and col_idx == 2:
+                    tmp[0][1] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 1 and col_idx == 0:
+                    tmp[1][2] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 1 and col_idx == 1:
+                    tmp[1][0] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 1 and col_idx == 2:
+                    tmp[1][1] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 2 and col_idx == 0:
+                    tmp[2][2] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 2 and col_idx == 1:
+                    tmp[2][0] = self.mobject_pos[row_idx][col_idx]
+                if row_idx == 2 and col_idx == 2:
+                    tmp[2][1] = self.mobject_pos[row_idx][col_idx]
+            for row_idx, col_idx in it.product(range(3), range(3)):
+                self.mobject_pos[row_idx][col_idx] = tmp[row_idx][col_idx]
+
+
+class shift_chess_betw_boards(Scene):
     def construct(self):
         s = NineSquare(self, chess_pos = [
-            [0, 1, 2],
+            [0, 3, 4],
             [0, 0, 3],
-            [1, 0, 0],
+            [2, 0, 0],
             ], square_h=1.5, square_w=1.5)
         s.move_to(LEFT*4)
         s.display()
@@ -242,22 +423,42 @@ class nine_square_problem(Scene):
 
         self.play(
             *t.add_chess_to_board(chess_pos = [
-                    [0, 1, 1],
-                    [0, 0, 2],
+                    [0, 2, 2],
+                    [0, 0, 3],
                     [1, 0, 0],
                 ], return_animate = True, source_board = s),
         )
         self.wait()
 
-        # a = [
-            # [[], [], []],
-            # [[], [], []],
-            # [[], [], []],
-        # ]
-        # for row_idx, col_idx in it.product(range(3), range(3)):
-            # for obj in t.mobject_pos[row_idx][col_idx]:
-                # a[row_idx][col_idx].append(obj.z_index)
 
-        # print(a)
+class board_rotate(Scene):
+    def construct(self):
+        s = NineSquare(self, chess_pos = [
+            [0, 3, 4],
+            [0, 0, 3],
+            [2, 0, 0],
+            ], square_h=1.5, square_w=1.5)
+        s.move_to(LEFT*4)
+        s.display()
+
+        self.play(
+            *s.board_rotate(return_animate=True)
+        )
+        self.wait()
+
+class chess_left_shift(Scene):
+    def construct(self):
+        s = NineSquare(self, chess_pos = [
+            [0, 3, 4],
+            [0, 0, 3],
+            [2, 0, 0],
+            ], square_h=1.5, square_w=1.5)
+        s.move_to(LEFT*4)
+        s.display()
+
+        self.play(
+            *s.chess_left_shift(return_animate=True)
+        )
+        self.wait()
 
 
